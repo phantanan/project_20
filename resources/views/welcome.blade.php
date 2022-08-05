369fd335713092a4c78aaef6e658890337b61ec4
Aidtisak <chaleezsaver@gmail.com>
2022-08-05 10:18:10 +0700

    แก้ใหม่ล่าสุด
    aaaa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html> aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
