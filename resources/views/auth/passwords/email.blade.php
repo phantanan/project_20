8e56b9b42f498179fc2ecc12d2bdf00adfe95040
phantanan <psckliankan@gmail.com>
2022-08-07 20:05:24 +0700

    Update email.blade.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,39 @@
-@extends('layouts.app')
+@extends('layouts.admin.master')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+@section('text')
+<form class="login100-form validate-form" method="POST" action="{{ route('password.email') }}">
+					@csrf
+					<span class="login100-form-title">
+					Reset Password
+					</span>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
+					<div class="wrap-input100 validate-input" data-validate = "Enter email">
+						<input class="input100 form-control @error('email') is-invalid @enderror" type="email" name="email" placeholder="email">
+                        
+                            @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+                             @enderror
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+                        Send Password Reset Link
+						</button>
+					</div>
+
+					<div class="text-center p-t-40">
+						<a class="txt2" href="../login">
+                        <i class="fa fa-long-arrow-left m-l-5" aria-hidden="true"></i>
+							login your Account
+						</a>
+					</div>
+				</form>
+
 @endsection
