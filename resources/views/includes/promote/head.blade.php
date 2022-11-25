57342112a0773b6883e5f34d6df109f7e30bac11
phantanan <psckliankan@gmail.com>
2022-11-25 09:56:06 +0700

    25112022

--- resources/views/includes/promote/head.blade.php
+++ resources/views/includes/promote/head.blade.php
@@ -7,9 +7,9 @@
     <!-- Favicon -->
     <link href="{{ asset('template/promote/img/logoicon.png')}}" rel="icon">
 
-    <!-- Google Font -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"> 
+    <!-- Google Font old-->
+    <!-- <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">  -->
 
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
@@ -21,3 +21,7 @@
     <!-- Customized Bootstrap Stylesheet -->
     <link href="{{ asset('template/promote/css/style.css')}}" rel="stylesheet">
 
+    <!-- fonts google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Itim&family=Yanone+Kaffeesatz&display=swap" rel="stylesheet">
